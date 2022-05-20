ab53a6785d7927fe1459b1be3a8c7219f038164c
elvis <qwjlu@sina.com>
2022-05-20 14:17:52 +0800

    result

--- collection/go.mod
+++ collection/go.mod
@@ -1,3 +1,5 @@
 module github.com/qianwj/typed/collection
 
 go 1.18
+
+require github.com/qianwj/typed/core v0.0.5 // indirect
--- collection/operation.go
+++ collection/operation.go
@@ -1,9 +1,23 @@
 package collection
 
+import "github.com/qianwj/typed/core/result"
+
 func Map[T, U any](input []T, convert func(t T) U) []U {
 	res := make([]U, len(input))
 	for i, t := range input {
 		res[i] = convert(t)
 	}
 	return res
+}
+
+func MapResult[T, U any](input []T, convert func(t T) (U, error)) result.Result[[]U] {
+	res := make([]U, len(input))
+	var err error
+	for i, t := range input {
+		res[i], err = convert(t)
+		if err != nil {
+			return result.Err[[]U](err)
+		}
+	}
+	return result.Ok[[]U](res)
 }
