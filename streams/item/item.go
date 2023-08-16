7cc31566ea7470d293729a2ada0fd5110b7fe61a
elvis <qwjlu@sina.com>
2023-08-16 23:36:32 +0800

    stream item

---
+++ streams/item/item.go
@@ -0,0 +1,34 @@
+package item
+
+func Ok(data any) *Item {
+	return &Item{data: data}
+}
+
+func Err(err error) *Item {
+	return &Item{error: err}
+}
+
+type Item struct {
+	data  any
+	error error
+}
+
+func (i Item) IsOk() bool {
+	return i.error == nil
+}
+
+func (i Item) IsErr() bool {
+	return i.error != nil
+}
+
+func (i Item) Data() any {
+	return i.data
+}
+
+func (i Item) Err() error {
+	return i.error
+}
+
+func (i Item) Get() (any, error) {
+	return i.data, i.error
+}
