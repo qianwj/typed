998413ccc1f609723725d4c5694cac51e41a7dd9
elvis <qwjlu@sina.com>
2023-08-20 23:54:50 +0800

    backpressure?

--- streams/consumer.go
+++ streams/consumer.go
@@ -2,12 +2,15 @@ package streams
 
 import (
 	"context"
+	"time"
 )
 
 type FunctionalSubscriber struct {
 	consumer          func(any)
 	errConsumer       func(error)
 	completionHandler func()
+	consumes          int
+	complete          bool
 	ctx               context.Context
 }
 
@@ -19,11 +22,23 @@ func NewFunctionalSubscriber(consumer func(any), errConsumer func(error)) Subscr
 }
 
 func (f *FunctionalSubscriber) OnSubScribe(sub Subscription) {
-	sub.Request(1)
+	tick := time.NewTicker(time.Second * 2)
+	for !f.complete {
+		<-tick.C
+		if f.consumes < 1 {
+			// slow
+			sub.Request(1)
+		} else {
+			sub.Request(10)
+		}
+		f.consumes = 0
+	}
+	tick.Stop()
 }
 
 func (f *FunctionalSubscriber) OnNext(val any) {
 	f.consumer(val)
+	f.consumes++
 }
 
 func (f *FunctionalSubscriber) OnError(e error) {
@@ -33,6 +48,7 @@ func (f *FunctionalSubscriber) OnError(e error) {
 }
 
 func (f *FunctionalSubscriber) OnComplete() {
+	f.complete = true
 	if f.completionHandler != nil {
 		f.completionHandler()
 	}
