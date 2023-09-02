f5cc60a1c2a79c1e3ceddf3bb413277f51755eff
elvis <qwjlu@sina.com>
2023-09-02 10:24:17 +0800

    overwrite

--- mongo/bson/map.go
+++ mongo/bson/map.go
@@ -2,11 +2,13 @@ package bson
 
 import (
 	"encoding/json"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 type Map interface {
 	Bson
 	json.Marshaler
+	bson.Marshaler
 	Put(string, Bson) Map
 	PutAll(other Map)
 	Get(key string) (Bson, bool)
