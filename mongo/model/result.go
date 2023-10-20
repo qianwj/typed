2da59db6c0190a7eecf77e193e63b39a78609f76
elvis <qwjlu@sina.com>
2023-10-21 00:16:14 +0800

    result

--- mongo/model/result.go
+++ mongo/model/result.go
@@ -1,15 +1,18 @@
 package model
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"github.com/qianwj/typed/mongo/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
-type UpdateResult[I DocumentId] struct {
+type UpdateResult[I bson.ID] struct {
 	MatchedCount  int64 // The number of documents matched by the filter.
 	ModifiedCount int64 // The number of documents modified by the operation.
 	UpsertedCount int64 // The number of documents upserted by the operation.
 	UpsertedID    I     // The _id field of the upserted document, or nil if no upsert was done.
 }
 
-func FromUpdateResult[I DocumentId](r *mongo.UpdateResult) *UpdateResult[I] {
+func FromUpdateResult[I bson.ID](r *mongo.UpdateResult) *UpdateResult[I] {
 	if r == nil {
 		return nil
 	}
@@ -22,7 +25,7 @@ func FromUpdateResult[I DocumentId](r *mongo.UpdateResult) *UpdateResult[I] {
 }
 
 // BulkWriteResult is the result type returned by a BulkWrite operation.
-type BulkWriteResult[I DocumentId] struct {
+type BulkWriteResult[I bson.ID] struct {
 	// The number of documents inserted.
 	InsertedCount int64
 
@@ -42,7 +45,7 @@ type BulkWriteResult[I DocumentId] struct {
 	UpsertedIDs map[int64]I
 }
 
-func FromBulkWriteResult[I DocumentId](r *mongo.BulkWriteResult) *BulkWriteResult[I] {
+func FromBulkWriteResult[I bson.ID](r *mongo.BulkWriteResult) *BulkWriteResult[I] {
 	if r == nil {
 		return nil
 	}
