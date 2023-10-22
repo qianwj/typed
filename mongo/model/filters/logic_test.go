d31fa3841a393929a9fd0450b8085ee806232d9d
elvis <qwjlu@sina.com>
2023-10-22 18:31:40 +0800

    logic test

--- mongo/model/filters/logic_test.go
+++ mongo/model/filters/logic_test.go
@@ -17,3 +17,14 @@ func TestAnd(t *testing.T) {
 	actual, _ := bson.Marshal(And(Eq("a", "b"), Eq("p", "q")))
 	assert.Equal(t, expected, actual)
 }
+
+func TestOr(t *testing.T) {
+	expected, _ := bson.Marshal(bson.D{
+		{operator.Or, bson.A{
+			bson.D{{"a", "b"}},
+			bson.D{{"p", "q"}},
+		}},
+	})
+	actual, _ := bson.Marshal(Or(Eq("a", "b"), Eq("p", "q")))
+	assert.Equal(t, expected, actual)
+}
