070bcd7c1c1f5cc76b16d121b7b98354b1d0e184
elvis <qwjlu@sina.com>
2023-10-24 17:24:14 +0800

    findone test

---
+++ mongo/executor/findone.go
@@ -0,0 +1,159 @@
+package executor
+
+import (
+	"context"
+	"github.com/qianwj/typed/mongo/bson"
+	"github.com/qianwj/typed/mongo/model/filters"
+	"github.com/qianwj/typed/mongo/model/projections"
+	"github.com/qianwj/typed/mongo/model/sorts"
+	"github.com/qianwj/typed/mongo/options"
+	rawbson "go.mongodb.org/mongo-driver/bson"
+	raw "go.mongodb.org/mongo-driver/mongo"
+	rawopts "go.mongodb.org/mongo-driver/mongo/options"
+	"time"
+)
+
+type FindOneExecutor[D bson.Doc[I], I bson.ID] struct {
+	readprefPrimary *raw.Collection
+	readprefDefault *raw.Collection
+	filter          *filters.Filter
+	primary         bool
+	opts            *rawopts.FindOneOptions
+}
+
+func NewFindOneExecutor[D bson.Doc[I], I bson.ID](readprefPrimary, readprefDefault *raw.Collection, filter *filters.Filter) *FindOneExecutor[D, I] {
+	return &FindOneExecutor[D, I]{
+		readprefPrimary: readprefPrimary,
+		readprefDefault: readprefDefault,
+		filter:          filter,
+		opts:            rawopts.FindOne(),
+	}
+}
+
+func (f *FindOneExecutor[D, I]) Primary() *FindOneExecutor[D, I] {
+	f.primary = true
+	return f
+}
+
+// AllowPartialResults sets the value for the AllowPartialResults field.
+func (f *FindOneExecutor[D, I]) AllowPartialResults() *FindOneExecutor[D, I] {
+	f.opts.SetAllowPartialResults(true)
+	return f
+}
+
+// Collation sets the value for the Collation field.
+func (f *FindOneExecutor[D, I]) Collation(collation *options.Collation) *FindOneExecutor[D, I] {
+	f.opts.SetCollation((*rawopts.Collation)(collation))
+	return f
+}
+
+// Comment sets the value for the Comment field.
+func (f *FindOneExecutor[D, I]) Comment(comment string) *FindOneExecutor[D, I] {
+	f.opts.SetComment(comment)
+	return f
+}
+
+// Hint sets the value for the Hint field.
+func (f *FindOneExecutor[D, I]) Hint(index string) *FindOneExecutor[D, I] {
+	f.opts.SetHint(index)
+	return f
+}
+
+func (f *FindOneExecutor[D, I]) Skip(skip int64) *FindOneExecutor[D, I] {
+	f.opts.SetSkip(skip)
+	return f
+}
+
+// Max sets the value for the Max field.
+func (f *FindOneExecutor[D, I]) Max(max int) *FindOneExecutor[D, I] {
+	f.opts.SetMax(max)
+	return f
+}
+
+// MaxTime specifies the max time to allow the query to run.
+func (f *FindOneExecutor[D, I]) MaxTime(d time.Duration) *FindOneExecutor[D, I] {
+	f.opts.SetMaxTime(d)
+	return f
+}
+
+// Min sets the value for the Min field.
+func (f *FindOneExecutor[D, I]) Min(min int) *FindOneExecutor[D, I] {
+	f.opts.SetMin(min)
+	return f
+}
+
+// ReturnKey sets the value for the ReturnKey field.
+func (f *FindOneExecutor[D, I]) ReturnKey() *FindOneExecutor[D, I] {
+	f.opts.SetReturnKey(true)
+	return f
+}
+
+// ShowRecordID sets the value for the ShowRecordID field.
+func (f *FindOneExecutor[D, I]) ShowRecordID() *FindOneExecutor[D, I] {
+	f.opts.SetShowRecordID(true)
+	return f
+}
+
+func (f *FindOneExecutor[D, I]) Sort(sort *sorts.Options) *FindOneExecutor[D, I] {
+	f.opts.SetSort(sort)
+	return f
+}
+
+func (f *FindOneExecutor[D, I]) Projection(projection *projections.Options) *FindOneExecutor[D, I] {
+	f.opts.SetProjection(rawbson.D{{"name", 1}})
+	return f
+}
+
+// MaxAwaitTime sets the value for the MaxAwaitTime field.
+func (f *FindOneExecutor[D, I]) MaxAwaitTime(d time.Duration) *FindOneExecutor[D, I] {
+	f.opts.SetMaxAwaitTime(d)
+	return f
+}
+
+// OplogReplay sets the value for the OplogReplay field.
+//
+// Deprecated: This option has been deprecated in MongoDB version 4.4 and will be ignored by the server if it is set.
+func (f *FindOneExecutor[D, I]) OplogReplay() *FindOneExecutor[D, I] {
+	f.opts.SetOplogReplay(true)
+	return f
+}
+
+// Snapshot sets the value for the Snapshot field.
+//
+// Deprecated: This option has been deprecated in MongoDB version 3.6 and removed in MongoDB version 4.0.
+func (f *FindOneExecutor[D, I]) Snapshot() *FindOneExecutor[D, I] {
+	f.opts.SetSnapshot(true)
+	return f
+}
+
+func (f *FindOneExecutor[D, I]) Execute(ctx context.Context) (D, error) {
+	var (
+		data D
+		res  *raw.SingleResult
+	)
+	if f.primary {
+		res = f.readprefPrimary.FindOne(ctx, f.filter, f.opts)
+	} else {
+		res = f.readprefDefault.FindOne(ctx, f.filter, f.opts)
+	}
+	if res.Err() != nil {
+		return data, res.Err()
+	}
+	if err := res.Decode(&data); err != nil {
+		return data, err
+	}
+	return data, nil
+}
+
+func (f *FindOneExecutor[D, I]) Collect(ctx context.Context, data any) error {
+	var res *raw.SingleResult
+	if f.primary {
+		res = f.readprefPrimary.FindOne(ctx, f.filter, f.opts)
+	} else {
+		res = f.readprefDefault.FindOne(ctx, f.filter, f.opts)
+	}
+	if res.Err() != nil {
+		return res.Err()
+	}
+	return res.Decode(data)
+}
---
+++ mongo/executor/findone_test.go
@@ -0,0 +1,70 @@
+package executor
+
+import (
+	"context"
+	"github.com/qianwj/typed/mongo/model/filters"
+	"github.com/qianwj/typed/mongo/model/projections"
+	"github.com/qianwj/typed/mongo/util"
+	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"testing"
+)
+
+func TestFindOneExecutor_Execute(t *testing.T) {
+	prepared := []*TestDoc{
+		{Id: primitive.NewObjectID(), Name: "test1", Age: 10},
+		{Id: primitive.NewObjectID(), Name: "test2", Age: 10},
+		{Id: primitive.NewObjectID(), Name: "test3", Age: 10},
+		{Id: primitive.NewObjectID(), Name: "test3", Age: 20},
+		{Id: primitive.NewObjectID(), Name: "test3", Age: 30},
+		{Id: primitive.NewObjectID(), Name: "test4", Age: 40},
+	}
+	_, _ = testColl.InsertMany(context.TODO(), util.ToAny(prepared))
+	t.Run("test one", func(t *testing.T) {
+		exec := NewFindOneExecutor[*TestDoc, primitive.ObjectID](testColl, testColl, filters.Eq("name", "test1"))
+		actual, err := exec.Execute(context.TODO())
+		if err != nil {
+			t.Errorf("find error: %+v", err)
+			t.FailNow()
+		}
+		assert.Equal(t, prepared[0], actual)
+	})
+	t.Run("test multi", func(t *testing.T) {
+		exec := NewFindOneExecutor[*TestDoc, primitive.ObjectID](testColl, testColl, filters.Eq("name", "test3").Eq("age", 30))
+		actual, err := exec.Execute(context.TODO())
+		if err != nil {
+			t.Errorf("find error: %+v", err)
+			t.FailNow()
+		}
+		assert.Equal(t, prepared[4], actual)
+	})
+	t.Run("test by id", func(t *testing.T) {
+		exec := NewFindOneExecutor[*TestDoc, primitive.ObjectID](testColl, testColl, filters.Eq("_id", prepared[3].Id))
+		actual, err := exec.Execute(context.TODO())
+		if err != nil {
+			t.Errorf("find error: %+v", err)
+			t.FailNow()
+		}
+		assert.Equal(t, prepared[3], actual)
+	})
+}
+
+func TestFindOneExecutor_Collect(t *testing.T) {
+	prepared := []*TestDoc{
+		{Id: primitive.NewObjectID(), Name: "test1", Age: 10},
+		{Id: primitive.NewObjectID(), Name: "test2", Age: 10},
+	}
+	_, _ = testColl.InsertMany(context.TODO(), util.ToAny(prepared))
+	t.Run("test one", func(t *testing.T) {
+		exec := NewFindOneExecutor[*TestDoc, primitive.ObjectID](testColl, testColl, filters.Eq("name", "test1")).
+			Projection(projections.Includes("name").ExcludeId())
+		var actual testDocProj
+		err := exec.Collect(context.TODO(), &actual)
+		if err != nil {
+			t.Errorf("find one error: %+v", err)
+			t.FailNow()
+		}
+		expected := &testDocProj{Name: "test1"}
+		assert.Equal(t, expected, &actual)
+	})
+}
